110597a845ec60e634b16485b896bae44de94019
Plimon Howard V <plimon.howard@gmail.com>
2020-09-21 21:02:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,14 @@ def all_supplies_in_holidays(holiday_hash)
           end
         end
         finl_string += wntrstring
+    elsif (key == :summer)
+      smrstring = "Summer:\n"
+      value.each do |holiday, item|
+        tmparry = holiday.to_s.split("_")
+        tmparry.each_with_index {|val,index| tmparry[index] = val.capitalize}
+        smrstring += (" " + tmparry.join(" ") + ": " +  item.join(",").to_s + "\n"
       end
+    end
   end
 end
 
